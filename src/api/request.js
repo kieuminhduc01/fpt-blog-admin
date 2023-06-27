652bd3d2d5d628d3c74868e0a027447ca909acae
bach <bach.tv2000@gmail.com>
2023-06-27 22:34:38 +0700

    fix ip

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-export const BASE_URL = 'http://103.229.41.235:99/'
+export const BASE_URL = 'http://103.101.160.70:99/'
 export const request = axios.create({
   baseURL: `${BASE_URL}api`,
   timeout: 20_000,
